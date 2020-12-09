93cd23a4a565677eecdadc0ab3475c29453c5e22
vikas-717 <75132264+vikas-717@users.noreply.github.com>
2020-12-10 00:48:50 +0530

    Add files via upload

--- module4_solutions/javascript/Script.js
+++ module4_solutions/javascript/Script.js
@@ -1,1 +1,16 @@
+(function () {
 
+    var names = ["Sai", "Vikas", "Reddy", "Siri", "Venkata", "Charan", "Matti", "Jake", "Johnny", "Rakshasi"];
+    
+    for (var a = 0; a < names.length; a++) {
+    
+      var firstLetter = names[a].charAt(0).toLowerCase();
+      if (firstLetter === 'j') {
+        byeSpeaker.speak(names[a]);
+      } 
+      else {
+        helloSpeaker.speak(names[a]);
+      }
+    }
+    
+    })();
